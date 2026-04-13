f72e477504d2c3468e0674f32c17d8fb6f8639f3
Johan Lopez <lukasdpikesevg@gmail.com>
2026-04-13 16:52:04 -0500

    implementamos clase Guest como dataclass con tipos por defecto y método __str__

--- app/model/hotel.py
+++ app/model/hotel.py
@@ -1,19 +1,14 @@
 from dataclasses import dataclass, field
-from datetime import datetime, date, timedelta
-from typing import ClassVar
-
-from app.services.util import (generate_unique_id, date_lower_than_today_error,
-    reservation_not_found_error, guest_not_found_error, room_not_available_error,
-    room_not_found_error, room_already_exists_error)
 
 
-# TODO: Implement Guest class here
+@dataclass
+class Guest:
+    REGULAR = "regular"
+    VIP = "vip"
 
+    name: str
+    email: str
+    type_: str = REGULAR
 
-# TODO: Implement Reservation class here
-
-
-# TODO: Implement Room class here
-
-
-# TODO: Implement Hotel class here
+    def __str__(self) -> str:
+        return f"Guest {self.name} ({self.email}) of type {self.type_}"
