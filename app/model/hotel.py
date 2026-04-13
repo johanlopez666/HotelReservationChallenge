42ad002973fa05a3e631f436099111746e3f1c98
Johan Lopez <lukasdpikesevg@gmail.com>
2026-04-13 17:02:42 -0500

    Implementamos clase Reservation con generación automática de id y manejo de huéspedes

--- app/model/hotel.py
+++ app/model/hotel.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass, field
+from datetime import date, datetime, timedelta
 
+from app.services.util import (
+    generate_unique_id,
+    guest_not_found_error
+)
 
 @dataclass
 class Guest:
@@ -12,3 +17,32 @@ class Guest:
 
     def __str__(self) -> str:
         return f"Guest {self.name} ({self.email}) of type {self.type_}"
+
+
+@dataclass
+class Reservation:
+    guest_name: str
+    description: str
+    check_in: date
+    check_out: date
+    guests: list[Guest] = field(default_factory=list)
+    id: str = field(default_factory=generate_unique_id)
+
+    def add_guest(self, name: str, email: str, type_: str = Guest.REGULAR):
+        new_guest = Guest(name=name, email=email, type_=type_)
+        self.guests.append(new_guest)
+
+    def delete_guest(self, guest_index: int):
+        if 0 <= guest_index < len(self.guests):
+            self.guests.pop(guest_index)
+        else:
+            guest_not_found_error()
+
+    def __len__(self) -> int:
+        return (self.check_out - self.check_in).days
+
+    def __str__(self) -> str:
+        return (f"ID: {self.id}\n"
+                f"Guest: {self.guest_name}\n"
+                f"Description: {self.description}\n"
+                f"Dates: {self.check_in} - {self.check_out}")
